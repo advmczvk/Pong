2184ace35a06e3eecb443bd7a8c328da663e2466
Kuba Adamczyk <kuba.adamczyk99@gmail.com>
2019-10-27 01:45:23 +0200

    Init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle paddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball);
+        Instantiate(paddle);
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 position;
+    bool isDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        if(this.name == "PaddleDown")
+        {
+            isDown = true;
+            rb.AddForce(position);
+            
+        }
+        else
+        {
+            isDown = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDown)
+        {
+            //if(Input.get)
+        }
+    }
+}
